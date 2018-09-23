ede5027f8d4e71150f72a4f2b0a2351e683bf835
Tyler G <guthriet1230@gmail.com>
2018-09-23 15:35:59 -0500

    progress 1

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,37 +1,36 @@
-var targetNumber = 53;
+// Generate a random number that we need to reach
+// -----------------------------------------------
+var targetNumber;
+targetNumber = Math.floor(Math.random() * 120) + 19;
+$("#random-number").text(targetNumber);
+// console.log(targetNumber)
 
-$("#number-to-guess").text(targetNumber);
 
+// Generate a number that we need to assign to our cards
+// -----------------------------------------------
 var counter = 0;
+var numberOptions = Math.floor(Math.random() * 12) +1;
+// console.log(numberOptions);
+ 
 
-var numberOptions = [10, 5, 3, 7];
+// Assign the random number to our card values
+// -----------------------------------------------
+$(".card").attr("data-cardvalue", numberOptions);
 
-for (var i = 0; i < numberOptions.length; i++) {
-  var imageCrystal = $("<img>");
 
-  imageCrystal.addClass("crystal-image");
-
-  imageCrystal.attr(
-    "src",
-    "http://cdn.playbuzz.com/cdn/35910209-2844-45c0-b099-f4d82878d54f/00261fda-4062-4096-81fd-8cf96b9034e8.jpg"
-  );
-
-  imageCrystal.attr("data-crystalvalue", numberOptions[i]);
-
-  $("#crystals").append(imageCrystal);
-}
-
-$(".crystal-image").on("click", function() {
-  var crystalValue = $(this).attr("data-crystalvalue");
-  crystalValue = parseInt(crystalValue);
-
-  counter += crystalValue;
-
+// assign an on click function that adds your scores together and displays it on 'your score'
+// -----------------------------------------------=
+$(".card").on("click", function() {
+  var cardValue = ($(this).attr("data-cardvalue"));
+  cardValue = parseInt(cardValue);
+  counter += cardValue;
   alert("New score: " + counter);
 
   if (counter === targetNumber) {
     alert("You win!");
-  } else if (counter >= targetNumber) {
+  }
+
+  else if (counter >= targetNumber) {
     alert("You lose!!");
   }
 });
