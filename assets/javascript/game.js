58ff4dbedaba1a8df87d521578a6bdbf08e61824
Tyler G <guthriet1230@gmail.com>
2018-09-24 22:57:27 -0500

    functionality complete

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,36 +1,62 @@
-// Generate a random number that we need to reach
-// -----------------------------------------------
-var targetNumber;
-targetNumber = Math.floor(Math.random() * 120) + 19;
-$("#random-number").text(targetNumber);
-// console.log(targetNumber)
+////////////// Global Variables //////////////////
+//////////////////////////////////////////////////
 
+var random_result;
+var loses = 0;
+var wins = 0;
+var totalScore = 0;
+var heart = $("#heart");
+var club = $("#club");
+var spade = $("#spade");
+var diamond = $("#diamond");
 
-// Generate a number that we need to assign to our cards
-// -----------------------------------------------
-var counter = 0;
-var numberOptions = Math.floor(Math.random() * 12) +1;
-// console.log(numberOptions);
- 
+var cards = [spade, heart, club, diamond];
 
-// Assign the random number to our card values
-// -----------------------------------------------
-$(".card").attr("data-cardvalue", numberOptions);
+//////////// StartGame Function //////////////////
+//////////////////////////////////////////////////
+function startGame() {
+  totalScore = 0;
+  $("#loses-score").html(loses);
+  $("#wins-score").html(wins);
 
+  $(".card").empty();
+  random_result = Math.floor(Math.random() * 102) + 19;
+  $("#random-number").text(random_result);
+  console.log("random number = " + random_result);
+  $("#your-number").text(totalScore);
 
-// assign an on click function that adds your scores together and displays it on 'your score'
-// -----------------------------------------------=
-$(".card").on("click", function() {
-  var cardValue = ($(this).attr("data-cardvalue"));
-  cardValue = parseInt(cardValue);
-  counter += cardValue;
-  alert("New score: " + counter);
-
-  if (counter === targetNumber) {
-    alert("You win!");
+  // asigning a random number to each card
+  for (var i = 0; i < 4; i++) {
+    var random = Math.floor(Math.random() * 12) + 1;
+    console.log(random);
+    cards[i].attr({
+      "data-random": random
+    });
   }
+}
 
-  else if (counter >= targetNumber) {
-    alert("You lose!!");
+//////////// On Click Function //////////////////
+//////////////////////////////////////////////////
+$(".card").on("click", function() {
+  var num = parseInt($(this).attr("data-random"));
+  totalScore += num;
+
+  $("#your-number").text(totalScore);
+  console.log(totalScore);
+
+  if (totalScore > random_result) {
+    loses++;
+    alert("Hah! Loser");
+    $("#loses-score").html(loses);
+    startGame();
+  } else if (totalScore === random_result) {
+    wins++;
+    alert("Winner-winner, chicken-dinner!");
+    $("#wins-score").html(wins);
+    startGame();
   }
 });
+
+//////////// Calling the StartGame Function //////////////////
+/////////////////////////////////////////////////////////////
+startGame();
